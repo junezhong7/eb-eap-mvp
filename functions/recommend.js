95b92a1792aa685dba4bb8bfc12bbbef23a19a44
Keith Zhong <kezhong@wlgore.com>
2026-03-16 14:03:43 +0800

    Added rules and resource list

--- functions/recommend.js
+++ functions/recommend.js
@@ -1,21 +1,15 @@
+const rules = require('./rules.json');
+
 module.exports = async function (request, context) {
     const q1 = request.query.get('q1');
     const q2 = request.query.get('q2');
-    const q3 = request.query.get('q3');
 
-    const responseData = {
-        resources: []
-    };
+    const key = `${q1}_${q2}`;
+    const rule = rules[key];
 
-    if (q1 === "a1" && q2 === "b1" && q3 === "c1") {
-        responseData.resources = [
-            {
-                title: "Base on your circumstance, we recommend you to check out followwing resources about how to manage work stress.",
-                link: "https://emotionalbalance.sharepoint.com/:b:/s/ResourceCenter/IQDgPz6sd0DVS7DUijR-kGzAATR-T84l2OmarbxHTwZA70Q?e=2tJ0c3",
-                linkText: "Click here to view the resource" 
-            }
-        ];
-    }
+    const responseData = rule
+        ? { title: rule.title, links: rule.links }
+        : { title: null, links: [] };
 
     return {
         status: 200,
